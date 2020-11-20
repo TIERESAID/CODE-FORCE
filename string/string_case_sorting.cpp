10515f085eb130482b96cebaafe36aa92af8c8cb
TIERESAID <vendeurn1@gmail.com>
2020-11-20 02:58:58 +0100

    sorting string using optimozed bubble sort

--- string/string_case_sorting.cpp
+++ string/string_case_sorting.cpp
@@ -9,16 +9,20 @@ using namespace std;
  }
 std::string case_sort(std::string str , int n ){
     
-    int min_index ;
+    bool swapped = true;
     for (int i = 0; i < n; ++i)
     {
-        min_index = i ;
-        for (int j = i+1; j < n; ++j)
+        swapped = false ;
+        for (int j = 0; j < n-i-1; ++j)
         {
-            if(str[j]<str[min_index])
-                min_index = j;
+            if(str[j]>str[j+1]){
+                swap(str[j],str[j+1]);
+                swapped = true;
+            }
+        
         }
-         swap(str[i],str[min_index]);
+         if(swapped == false)
+            break;
     }
 
     return str;
